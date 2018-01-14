46191dc428fe7db37b20e560106a4f398c2c17e3
Hitendra Singh <hitendra.singh@Admins-MacBook-Pro.local>
2018-01-14 16:20:44 +0530

    first commit

---
+++ CaptureVideoPOC/Effects/FocusEffect.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using CaptureVideoPOC.Effects;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ResolutionGroupName("GlobalLogic")]
+[assembly: ExportEffect(typeof(FocusEffect), "FocusEffect")]
+namespace CaptureVideoPOC.Effects
+{
+
+    public class FocusEffect : PlatformEffect
+    {
+        UIColor backgroundColor;
+        public FocusEffect()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                Control.BackgroundColor = backgroundColor = UIColor.Red;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            throw new NotImplementedException();
+        }
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (Control.BackgroundColor == backgroundColor)
+                    {
+                        Control.BackgroundColor = UIColor.White;
+                    }
+                    else
+                    {
+                        Control.BackgroundColor = backgroundColor;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+    }
+}
---
+++ CaptureVideoPOC/Views/CameraPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CaptureVideoPOC
+{
+	public partial class CameraPage : ContentPage
+	{
+		public CameraPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
